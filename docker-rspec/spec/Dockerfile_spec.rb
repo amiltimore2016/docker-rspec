36787aadf37338a397b363cbfa7ef89b338ada1f
amiltimore2016 <3soft4you@gmail.com>
2020-05-15 08:15:59 +0100

    Init repo

---
+++ docker-rspec/spec/Dockerfile_spec.rb
@@ -0,0 +1,27 @@
+# spec/Dockerfile_spec.rb
+
+require "serverspec"
+require "docker"
+
+describe "Dockerfile" do
+  before(:all) do
+    image = Docker::Image.build_from_dir('.')
+
+    set :os, family: :debian
+    set :backend, :docker
+    set :docker_image, image.id
+  end
+
+  it 'installs the right version of Ubuntu' do
+    expect(os_version).to include("Ubuntu 18")
+  end
+
+  it 'installs the required packages' do
+    expect(package("nodejs")).to be_installed
+  end
+
+  def os_version
+    command("cat /etc/issue").stdout
+  end
+end
+
